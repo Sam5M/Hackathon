64d9a16b4ed2ef5f40794f18c2e2a627f6fc6682
Sam5M <samuel.michaud@go.winona.edu>
2026-02-28 13:27:51 -0600

    Revert "Merge branch 'main' of https://github.com/Sam5M/Hackathon"
    This reverts commit 5c5ec727965d68d809770ccb39fb1ba4ceaec604, reversing
    changes made to 59ee20d9860d30dcf25d7e1e9f1f28518b375507.

--- gui.py
+++ gui.py
@@ -1,17 +1,5 @@
 from tkinter import *
 from tkinter import ttk
-import main
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-mn = main()
-ttk.Frame
-T = Text(Tk(), height=50, width=150)
-T.insert(END, print(main.subprocess.getoutput(main.cmd)))
-T.insert(END, print(main.json_data["response"], end="", flush=True))
-root.mainloop()
 from AI import AI
 
 class Gui:
