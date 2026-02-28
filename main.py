0e91a1704e2cc06899c6c590931d69ab61775286
xXEmmaPlayzXx <emmawendorf2@gmail.com>
2026-02-28 11:44:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import subprocess
+import tkinter
+from tkinter import *
+from tkinter import ttk
 # Source - https://stackoverflow.com/a/57439663
 # Posted by Ronn Macc, modified by community. See post 'Timeline' for change history
 # Retrieved 2026-02-28, License - CC BY-SA 4.0
@@ -7,6 +10,12 @@ import subprocess
 user = ""
 host = ""
 cmd = "cd.."
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
 
 # Python 2
 subprocess.Popen("ssh {user}@{host} {cmd}".format(user=user, host=host, cmd='ls -l'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
