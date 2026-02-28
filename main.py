bf6e371846c0120e23a3de9be126edd3bd83a2b4
ELarWSU <181546842+ELarWSU@users.noreply.github.com>
2026-02-28 10:31:46 -0600

    upload main

---
+++ main.py
@@ -0,0 +1,17 @@
+import subprocess
+# Source - https://stackoverflow.com/a/57439663
+# Posted by Ronn Macc, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-02-28, License - CC BY-SA 4.0
+
+#michael please enter your login info below in the variables
+user = ""
+host = ""
+cmd = "cd.."
+
+# Python 2
+subprocess.Popen("ssh {user}@{host} {cmd}".format(user=user, host=host, cmd='ls -l'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+# Python 3
+subprocess.Popen(f"ssh {user}@{host} {cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+print(subprocess.getoutput(cmd))
