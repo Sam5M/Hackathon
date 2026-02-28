d475d90876e7709e64983f6f236e16a1c5986ca9
Michael Hrstka <theGeigus@gmail.com>
2026-02-28 11:34:10 -0600

    Request now streams it's responses.

--- main.py
+++ main.py
@@ -16,8 +16,13 @@ cmd = "cd .."
 # subprocess.Popen(f"ssh {user}@{host} {cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
 print(subprocess.getoutput(cmd))
-response = requests.get(f"{host}tags")
+response = requests.post(url=f"{host}generate", data='{"model": "gemma3", "prompt": "Why is the sky blue"}', stream=True)
 
-print(response.json())
+# print(response.status_code, response.reason)
+# print(response.text)
+
+for line in response.iter_lines():
+    if line:
+        print(line)
 
 # To chat: curl http://164.152.25.35:11434/api/generate -d '{"model": "gemma3", "prompt": "Why is the sky blue?"}'
