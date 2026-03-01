1ab58fe50653734a022419a5f9354800ea31a00e
ELarWSU <181546842+ELarWSU@users.noreply.github.com>
2026-02-28 18:21:39 -0600

    still fixing main

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
 import subprocess
 import requests
 import json
-# Source - https://stackoverflow.com/a/57439663
-# Posted by Ronn Macc, modified by community. See post 'Timeline' for change history
-# Retrieved 2026-02-28, License - CC BY-SA 4.0
+from gui import gui
 
-#michael please enter your login info below in the variables
 user = ""
 host = "http://164.152.25.35:11434/api/"
 cmd = "cd .."
 
-# # Python 2
-# subprocess.Popen("ssh {user}@{host} {cmd}".format(user=user, host=host, cmd='ls -l'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-
-# # Python 3
-# subprocess.Popen(f"ssh {user}@{host} {cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-
 print(subprocess.getoutput(cmd))
-question = input("Enter question: ")
-response = requests.post(url=f"{host}generate", data=f'{{"model": "gemma3", "prompt": "{question}"}}', stream=True)
+response = requests.post(url=f"{host}generate", data='{"model": "gemma3", "prompt": "Why is the sky blue"}', stream=True)
 
 # print(response.status_code, response.reason)
 # print(response.text)
 
+
 for line in response.iter_lines():
     if line:
         json_data = json.loads(line)
-        print(json_data["response"], end="", flush=True)
+        print(json_data["response"], end="")
+
+gui = gui()
 
 # To chat: curl http://164.152.25.35:11434/api/generate -d '{"model": "gemma3", "prompt": "Why is the sky blue?"}'
