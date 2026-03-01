12eaa9e3924d67f6b56e73b559d5ee074ed8891a
ELarWSU <181546842+ELarWSU@users.noreply.github.com>
2026-02-28 20:40:19 -0600

    gui update
    update gui

--- gui.py
+++ gui.py
@@ -16,9 +16,23 @@ class Gui:
         frm.grid()
         self.responselabel = ttk.Label(frm, text="")
         self.responselabel.grid(column=0,row=1)
-        ai = AI(gui=self)
+        self.quitbutton =  ttk.Button(frm, text="Quit", command=self.root.destroy)
+        self.quitbutton.grid(column=0, row=2)
+
+
+        self.lblinput = ttk.Label(frm, text="Ask the AI a question below")
+        self.lblinput.grid(column=1, row=0)
+        self.entry = ttk.Entry(frm)
+        self.entry.grid(column=1, row = 1)
+        self.submit = ttk.Button(frm, text="Submit", command=self.submitEntry)
+        self.submit.grid(column=1, row=2)
+
+        self.ai = AI(gui=self)
         self.root.mainloop()
 
     #response = list
     def addResponse(self, response):
         self.responses.append(response)
+
+    def submitEntry(self):
+        self.ai.sendMessage(self.entry.get())
