af391d3df0751ecebeea967cd0f18134a7ffd749
ELarWSU <181546842+ELarWSU@users.noreply.github.com>
2026-02-28 18:20:15 -0600

    fix main
    main = main now and gui is a class

--- gui.py
+++ gui.py
@@ -1,11 +1,21 @@
 from tkinter import *
 from tkinter import ttk
 import main
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-mn = main()
-ttk.Frame
-root.mainloop()
+
+class gui:
+    def __init__(self):
+        #member variables = self...
+        self.responses = []
+        self.root = Tk()
+
+        self.root.geometry("800x600")
+
+        frm = ttk.Frame(self.root, padding=10)
+        ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+        ttk.Button(frm, text="Quit", command=self.root.destroy).grid(column=1, row=0)
+
+        self.root.mainloop()
+
+    #response = list
+    def addResponse(self, response):
+        self.responses.append(response)
