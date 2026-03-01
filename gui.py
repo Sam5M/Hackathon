a64e7f1c3abc3c85804a6da8f3c0489a82c2aac2
ELarWSU <181546842+ELarWSU@users.noreply.github.com>
2026-02-28 18:40:08 -0600

    update everything
    break things into more classes

--- gui.py
+++ gui.py
@@ -1,18 +1,24 @@
 from tkinter import *
 from tkinter import ttk
-import main
+from ai import ai
 
-class gui:
+class Gui:
     def __init__(self):
         #member variables = self...
         self.responses = []
         self.root = Tk()
-
-        self.root.geometry("800x600")
+        
+        self.root.geometry("600x600")
 
         frm = ttk.Frame(self.root, padding=10)
         ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
         ttk.Button(frm, text="Quit", command=self.root.destroy).grid(column=1, row=0)
+        ai = ai()
+        self.count = 0
+        self.responses = ai.getResponses()
+        for i in self.responses:
+            self.count += 1
+            ttk.Label(frm, text=i).grid(column=0, row=self.count)
 
         self.root.mainloop()
 
--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-import subprocess
-import requests
-import json
-from gui import gui
+from Gui import Gui
 
-user = ""
-host = "http://164.152.25.35:11434/api/"
-cmd = "cd .."
-
-print(subprocess.getoutput(cmd))
-response = requests.post(url=f"{host}generate", data='{"model": "gemma3", "prompt": "Why is the sky blue"}', stream=True)
-
-# print(response.status_code, response.reason)
-# print(response.text)
-
-
-for line in response.iter_lines():
-    if line:
-        json_data = json.loads(line)
-        print(json_data["response"], end="")
-
-gui = gui()
-
-# To chat: curl http://164.152.25.35:11434/api/generate -d '{"model": "gemma3", "prompt": "Why is the sky blue?"}'
+try:
+    gui = Gui()
+except Exception as e:
+    print(f"Error creating GUI: {e}")
